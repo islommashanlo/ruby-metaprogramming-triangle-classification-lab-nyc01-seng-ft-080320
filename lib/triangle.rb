57a324e5d9c06f6b3b068dae6807a7ef9d3ea36b
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-08-17 01:23:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,6 +16,14 @@ class Triangle
   end
 
   def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
   end
 
 end
